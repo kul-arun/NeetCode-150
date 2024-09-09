d84b990c0a7825f24b77d01f92f8471b280ebaf4
kul-arun <kul.arun21@gmail.com>
2024-09-09 20:12:57 +0200

    Add ValidParentheses.java

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = Map.ofEntries(
+                Map.entry('(', ')'),
+                Map.entry('{', '}'),
+                Map.entry('[', ']')
+        );
+        for (int i=0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.empty() || map.get(stack.pop()) != c) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
