24e1c2d155256d1718332ef6664c6f7cfb56fdbe
kul-arun <kul.arun21@gmail.com>
2024-09-16 13:26:59 +0200

    Add ReverseBits.java

---
+++ BitManipulation/ReverseBits.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/reverse-bits/
+
+public class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int i=31; i>=0; --i) {
+            ans |= (n&1)<<i;
+            n >>>= 1;
+        }
+        return ans;
+    }
+}
